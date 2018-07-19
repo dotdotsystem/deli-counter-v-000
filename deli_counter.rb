a69cc1b5ab31981a8ea3581994087f6edef9247d
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-19 20:48:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,17 @@ if deli_counter.empty?
   puts "The line is currently empty."
 else
  current_line = []
- return deli_counter.each_with_index {|name, index|}
+ return deli_counter.each_with_index do |name, index|
  current_line << "#{index + 1}. #{name}"
+ end
  puts "The line is currently: #{current_line.join(" ")}"
  end
 end
+
+
+
+
+
+"The line is currently: 1. Logan 2. Avi 3. Spencer"
+"The line is currently: 1. Logan 2. Avi 3. Spe
+ncer"
