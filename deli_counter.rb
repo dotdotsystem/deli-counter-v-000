4a3e7a6888236415c70283271c920d87b2323e17
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-19 20:43:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,12 @@
 
 def line(deli_counter)
-if deli_counter = []
-    return   "The line is currently empty."
-else 
-  when current_line = []
-  return deli_counter.each_with_index {|name, index|}
-current_line << "#{index = 1}" + name 
-   
-elsif
-  "The line is currently empty."
+
+if deli_counter.empty?
+  return   "The line is currently empty."
+else
+ current_line = []
+ return deli_counter.each_with_index {|name, index|}
+ current_line << "#{index + 1}. #{name}"
+ puts "The line is currently: #{current_line.join(" ")}"
+ end
 end
-end 
